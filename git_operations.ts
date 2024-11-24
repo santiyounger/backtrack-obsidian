9b093479775660315c76419796289c5b86112aa6
Santi <51902730+santiyounger@users.noreply.github.com>
2024-11-23 19:02:14 -0500

    removed comment

--- git_operations.ts
+++ git_operations.ts
@@ -1,6 +1,6 @@
 import { App, Notice, TFile } from "obsidian";
 import git from "isomorphic-git";
-import LightningFS from "@isomorphic-git/lightning-fs"; // Updated import
+import LightningFS from "@isomorphic-git/lightning-fs";
 import { diffLines } from "diff";
 
 const fs = new LightningFS("fs");
