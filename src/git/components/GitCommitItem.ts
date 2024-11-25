3e1b6dce2b5c8ac114476b5d52731d37643be7bd
Santi <51902730+santiyounger@users.noreply.github.com>
2024-11-25 14:34:17 -0500

    trying to clean things to remove useles files

---
+++ src/git/components/GitCommitItem.ts
@@ -0,0 +1,56 @@
+export class GitCommitItem {
+    private element: HTMLDivElement;
+    private static readonly TRUNCATION_LIMIT = 30;
+
+    constructor(
+        private commit: any,
+        private index: number,
+        private onSelect: (commit: any, index: number) => void
+    ) {
+        this.element = this.createCommitElement();
+    }
+
+    private createCommitElement(): HTMLDivElement {
+        const commitItem = document.createElement('div');
+        commitItem.className = 'commit-item';
+        
+        const date = new Date(this.commit.commit.author.timestamp * 1000);
+        const formattedDate = date.toISOString().split('T')[0].replace(/-/g, '/');
+        const formattedTime = date.toLocaleTimeString([], { 
+            hour: '2-digit', 
+            minute: '2-digit', 
+            hour12: false 
+        });
+
+        const message = this.truncateMessage(this.commit.commit.message);
+        
+        commitItem.innerHTML = `
+            <strong class="commit-date">${formattedDate} - ${formattedTime}</strong>
+            <div class="commit-details">
+                ${message}<br>
+            </div>
+        `;
+
+        commitItem.onclick = () => this.onSelect(this.commit, this.index);
+        
+        return commitItem;
+    }
+
+    private truncateMessage(message: string): string {
+        return message.length > GitCommitItem.TRUNCATION_LIMIT 
+            ? message.substring(0, GitCommitItem.TRUNCATION_LIMIT) + '...' 
+            : message;
+    }
+
+    getElement(): HTMLDivElement {
+        return this.element;
+    }
+
+    setSelected(selected: boolean): void {
+        if (selected) {
+            this.element.addClass('selected');
+        } else {
+            this.element.removeClass('selected');
+        }
+    }
+} 
--- src/git/components/GitSidebar.ts
+++ src/git/components/GitSidebar.ts
@@ -1,44 +1,46 @@
+import { GitCommitItem } from './GitCommitItem';
+
 export class GitSidebar {
-    constructor(private container: HTMLElement) {}
+    private sidebarElement: HTMLElement;
+    private commitList: HTMLElement;
+    private activeCommitItem: GitCommitItem | null = null;
+    private commitItems: GitCommitItem[] = [];
 
-    renderCommitList(commits: any[], onCommitSelect: (commit: any, index: number) => void) {
-        const sidebar = this.container.createDiv({ cls: 'git-sidebar' });
-        sidebar.setText('Commit History');
-        const commitList = sidebar.createDiv({ cls: 'commit-list' });
-
-        let activeCommitItem: HTMLDivElement | null = null;
-
-        commits.forEach((commit, index) => {
-            const commitItem = this.createCommitItem(commit, index);
-            commitList.appendChild(commitItem);
-
-            commitItem.onclick = () => {
-                if (activeCommitItem) {
-                    activeCommitItem.removeClass('selected');
-                }
-                commitItem.addClass('selected');
-                activeCommitItem = commitItem;
-                onCommitSelect(commit, index);
-            };
-
-            if (index === 0) {
-                commitItem.click();
-            }
-        });
+    constructor(private container: HTMLElement) {
+        this.initializeSidebar();
     }
 
-    private createCommitItem(commit: any, index: number): HTMLDivElement {
-        const commitItem = document.createElement('div');
-        commitItem.className = 'commit-item';
-        
-        const TRUNCATION_LIMIT = 30;
-        commitItem.innerHTML = `
-            <strong class="commit-date">${new Date(commit.commit.author.timestamp * 1000).toISOString().split('T')[0].replace(/-/g, '/')} - ${new Date(commit.commit.author.timestamp * 1000).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit', hour12: false })}</strong>
-            <div class="commit-details">
-                ${commit.commit.message.length > TRUNCATION_LIMIT ? commit.commit.message.substring(0, TRUNCATION_LIMIT) + '...' : commit.commit.message}<br>
-            </div>
-        `;
-        
-        return commitItem;
+    private initializeSidebar(): void {
+        this.sidebarElement = this.container.createDiv({ cls: 'git-sidebar' });
+        this.sidebarElement.setText('Commit History');
+        this.commitList = this.sidebarElement.createDiv({ cls: 'commit-list' });
+    }
+
+    renderCommitList(commits: any[], onCommitSelect: (commit: any, index: number) => void): void {
+        this.commitItems = commits.map((commit, index) => {
+            const commitItem = new GitCommitItem(commit, index, (commit, index) => {
+                this.handleCommitSelect(commitItem, commit, index, onCommitSelect);
+            });
+            this.commitList.appendChild(commitItem.getElement());
+            return commitItem;
+        });
+
+        if (this.commitItems.length > 0) {
+            this.commitItems[0].getElement().click();
+        }
+    }
+
+    private handleCommitSelect(
+        selectedItem: GitCommitItem, 
+        commit: any, 
+        index: number, 
+        onCommitSelect: (commit: any, index: number) => void
+    ): void {
+        if (this.activeCommitItem) {
+            this.activeCommitItem.setSelected(false);
+        }
+        selectedItem.setSelected(true);
+        this.activeCommitItem = selectedItem;
+        onCommitSelect(commit, index);
     }
 } 
