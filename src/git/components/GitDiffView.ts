bdf27315b2da88c4bd21252cefeaeee02f35b2e9
Santi <51902730+santiyounger@users.noreply.github.com>
2024-11-28 16:39:19 -0500

    simplifed message to retrigger

--- src/git/components/GitDiffView.ts
+++ src/git/components/GitDiffView.ts
@@ -19,10 +19,10 @@ export class GitDiffView {
         this.contentArea.addEventListener('click', () => {
             if (!this.isSelectionModeActive && !isNoticeVisible) {
                 isNoticeVisible = true;
-                new Notice('Please click on the `before` or `after` buttons to select text', 500);
+                new Notice('Please click on the `before` or `after` buttons to select text', 2500);
                 setTimeout(() => {
                     isNoticeVisible = false;
-                }, 500);
+                }, 2500);
             }
         });
     }
